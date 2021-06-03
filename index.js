aa69478c2d8a7a63dd14b1b2c9075bf9dea112fa
Daniel Bass <danielbass123456@gmail.com>
2021-06-02 22:55:05 -0500

    Error catching + User lookup changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  // client.channels.cache.get('849673551933472768').send("Hello! Message me for help!")
 });
 
 client.on('message', message => {
@@ -23,15 +24,27 @@ client.on('message', message => {
           .addField("User id", message.author.id)
           .setTimestamp(new Date())
       client.channels.cache.get(botConfig.channel).send(contactEmbed)
+          .then(s => {
+              console.log("Sent the embed!")
 
-      const replyEmbed = new Discord.MessageEmbed()
-          .setTitle("Message Recieved!")
-          .setColor(0x8cf5ab)
-          .setDescription(`Hello! Your message has been recieved and will be responded to as soon as possible.\
-                          Please refrain from sending multiple messages unless ABSOLUTELY necessary.\
-                          Abuse will result in a ban from the system.`)
-          .setTimestamp(new Date())
-      message.reply(replyEmbed)
+              const replyEmbed = new Discord.MessageEmbed()
+              .setTitle("Message Recieved!")
+              .setColor(0x8cf5ab)
+              .setDescription(`Hello! Your message has been recieved and will be responded to as soon as possible. Please refrain from sending multiple messages unless ABSOLUTELY necessary. Abuse will result in a ban from the system.`)
+              .setTimestamp(new Date())
+              message.reply(replyEmbed)
+                  .then(f => {
+                      console.log("Reply sent successfully.")
+                  })
+                  .catch(e => {
+                      console.error(e)
+                      message.reply("An error occured when replying to the user: " + e)
+                  })
+          })
+          .catch(e => {
+              console.error(e)
+              message.reply("Something went wrong when sending your message. Please try again later. If the problem persists, contact the bot owner.")
+          })
   }
 
   if (message.channel.id === botConfig.channel) {
@@ -40,13 +53,23 @@ client.on('message', message => {
               .then(m => {
                   let usrId = m.embeds[0].fields[2].value
 
+                  console.log(client.users.cache.get(usrId), usrId)
                   const dmEmbed = new Discord.MessageEmbed()
                       .setTitle("You have a reply!")
                       .setColor(0xfffafa)
                       .setDescription("Hello! Your message has been recieved and responded to by staff.")
                       .addField("Response", message.content)
                       .setTimestamp(new Date())
-                  client.users.cache.get(usrId).send(dmEmbed)
+                  client.users.fetch(usrId)
+                      .then(r => {
+                          r.send(dmEmbed)
+                      })
+                      .catch(e => {
+                          console.error(e)
+                      })
+              })
+              .catch(e => {
+                  console.error(e)
               })
       }
   }
