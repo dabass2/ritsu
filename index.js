006513e28bb0325badc1425462d8ea23c570d92f
Daniel Bass <danielbass123456@gmail.com>
2021-06-20 23:32:13 -0500

    Basic functions of the bot

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const botConfig = require("./ritsuConfig.json")
 const fs = require("fs")
 
 const Discord = require('discord.js');
-const { resolveSoa } = require("dns");
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -18,132 +17,170 @@ client.on('ready', () => {
     // fs.writeFileSync("./ritsuConfig.json", JSON.stringify(botConfig, null, 2));
 });
 
+client.on('guildMemberAdd', member => {
+    let role = member.guild.roles.cache.get('856374422063153193')
+
+    member.roles.add(role).catch((e) => {
+        console.error(e)
+    })
+})
+
 client.on('message', message => {
     let prefix = botConfig.prefix;
-    const args = message.content.slice(prefix.length).split(' ');
+
+    if (message.author.bot || message.content.slice(0, prefix.length) !== prefix) return;
+
+    const args = message.content.slice(prefix.length + 1).split(' ');
     const cmd = args.shift().toLowerCase();
-
-    if (message.author.bot) return;
-
-    if (message.channel.type === "dm") {
-        if (message.author.bot) return;
-
-        console.log(message.content, message.author.id)
-        const contactEmbed = new Discord.MessageEmbed()
-            .setTitle("Contact Message")
-            .setColor(0xffd4fe)
-            .setThumbnail(message.author.displayAvatarURL())
-            .addField("Message content", message.content)
-            .addField("User Name", message.author.username)
-            .addField("User id", message.author.id)
-            .setTimestamp(new Date())
-        client.channels.cache.get(botConfig.outputChannel).send(contactEmbed)
-            .then(s => {
-                console.log("Sent the embed!")
-
-                const replyEmbed = new Discord.MessageEmbed()
-                .setTitle("Message Recieved!")
-                .setColor(0x8cf5ab)
-                .setDescription(`Hello! Your message has been recieved and will be responded to as soon as possible. Please refrain from sending multiple messages unless ABSOLUTELY necessary. Abuse will result in a ban from the system.`)
-                .setTimestamp(new Date())
-                message.reply(replyEmbed)
-                    .then(f => {
-                        console.log("Reply sent successfully.")
-                    })
-                    .catch(e => {
-                        console.error(e)
-                        message.reply("An error occured when replying to the user: " + e)
-                    })
-            })
-            .catch(e => {
-                console.error(e)
-                message.reply("Something went wrong when sending your message. Please try again later. If the problem persists, contact the bot owner.")
-            })
-    }
-
-    if (message.channel.id === botConfig.outputChannel) {
-        if (message.mentions.has(client.user.id)) {
-            client.channels.cache.get(botConfig.outputChannel).messages.fetch(message.reference.messageID)
-                .then(m => {
-                    let usrId = m.embeds[0].fields[2].value
-
-                    console.log(client.users.cache.get(usrId), usrId)
-                    const dmEmbed = new Discord.MessageEmbed()
-                        .setTitle("You have a reply!")
-                        .setColor(0xfffafa)
-                        .setDescription("Hello! Your message has been recieved and responded to by staff.")
-                        .addField("Response", message.content)
-                        .setTimestamp(new Date())
-                    client.users.fetch(usrId)
-                        .then(r => {
-                            r.send(dmEmbed)
-                        })
-                        .catch(e => {
-                            console.error(e)
-                        })
-                })
-                .catch(e => {
-                    console.error(e)
-                })
-        }
-    }
+    // console.log(cmd, args)
 
     if (botConfig.adminUsers.includes(message.author.id)) {
-        if (args[0] === "setChannel") {
-            botConfig.outputChannel = message.channel.id
-            fs.writeFileSync("./ritsuConfig.json", JSON.stringify(botConfig, null, 2));
-            message.reply(`Set message output channel to be current channel. Id: ${message.channel.id}`)
+        if (cmd === "createnotifmessage") {
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x9af59f)
+                .setTitle("Get Roles Here")
+                .setDescription("React with the appropriate emote to get updates on that manga.")
+                .addField("Reincarnated as an Aristocrat with an Appraisal Skill", "1️⃣",)
+                .addField("Clearing the Isekai with a Zero-Believer Goddess", "2️⃣")
+                .addField("Breakthrough brought by Forbidden Master and Disciple", "3️⃣")
+                .setTimestamp(new Date())
+            message.channel.send(embed).then(async sent => {
+                await sent.react("1️⃣")
+                await sent.react("2️⃣")
+                await sent.react("3️⃣")
+                botConfig.roleMsg = sent.id
+                fs.writeFileSync("./ritsuConfig.json", JSON.stringify(botConfig, null, 2));
+            });
         }
 
-        if (args[0] === "createAlerts") {
-            const alertEmbed = new Discord.MessageEmbed()
-                .setTitle("Reacts")
-                .setDescription("React with the appropriate emote for whatever series you want to get alerts for.")
-                .addField("Appraisal", '1️⃣')
-                .addField("Shinja", '2️⃣')
-                .addField("Breakthrough", '3️⃣')
-                message.channel.send(alertEmbed).then(async sent => {
-                    await sent.react('1️⃣')
-                    await sent.react('2️⃣')
-                    await sent.react('3️⃣')
-                    const filter = (reaction) => {
-                      return reaction.emoji.name === '1️⃣' || reaction.emoji.name === '2️⃣' || reaction.emoji.name === '3️⃣';
-                    };
-                    let collector = new Discord.ReactionCollector(sent, filter, {dispose: true})
-                    collector.on("collect", (reaction, user) => {
-                        console.log(reaction.emoji.name, user.id)
-                        let emojiName = reaction.emoji.name
-                        // let user = user.id
-                        if (emojiName === '1️⃣') {
-                            let role = message.guild.roles.cache.get("849662899005292584")
-                            message.member.roles.add(role)
-                        } else if (emojiName === '2️⃣') {
-                            let role = message.guild.roles.cache.get("849663088763994112")
-                            message.member.roles.add(role)
-                        } else if (emojiName === '3️⃣') {
-                            let role = message.guild.roles.cache.get("849663164877504573")
-                            message.member.roles.add(role)
-                        }
-                    })
+        if (cmd === "appraisal") {
+            if (args.length < 1) {
+                message.reply("no new chapter link? dumbasss...LMFAO")
+                return;
+            }
 
-                    collector.on("remove", (reaction, user) => {
-                      console.log(reaction.emoji.name, user.id)
-                      let emojiName = reaction.emoji.name
-                      // let user = user.id
-                      if (emojiName === '1️⃣') {
-                          let role = message.guild.roles.cache.get("849662899005292584")
-                          message.member.roles.remove(role)
-                      } else if (emojiName === '2️⃣') {
-                          let role = message.guild.roles.cache.get("849663088763994112")
-                          message.member.roles.remove(role)
-                      } else if (emojiName === '3️⃣') {
-                          let role = message.guild.roles.cache.get("849663164877504573")
-                          message.member.roles.remove(role)
-                      }
-                  })
-                })
+            let chapterLink = args.splice(0, 1)
+            let comment = args.join(" ")
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x9af59f)
+                .setThumbnail("https://strapi.wehateisekai.club/uploads/51376_b55361b130.jpg")
+                .setTitle("New Chapter")
+                .setDescription("There's a new chapter of Appraisal!!!!!! OMG POG!!")
+                .addField("Read Chapter:", chapterLink)
+                .addField("Next Chapter Release", "~1 week")
+                .addField("Comment", (comment ? comment : "No upload comment because nothing cool happened."))
+                .setTimestamp(new Date())
+            client.channels.cache.get("856374920698265661").send(embed).then( () => {
+                client.channels.cache.get("856374920698265661").send(`<@&${"856375305921101836"}>`)
+            })
         }
+
+        if (cmd === "breakthrough") {
+            if (args.length < 1) {
+              message.reply("no new chapter link? dumbasss...LMFAO")
+              return;
+            }
+
+            let chapterLink = args.splice(0, 1)
+            let comment = args.join(" ")
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x9af59f)
+                .setThumbnail("https://strapi.wehateisekai.club/uploads/47371_e310fbcba5.jpg")
+                .setTitle("New Chapter")
+                .setDescription("There's a new chapter of Breakthrough!!!!!! OMG POG!!")
+                .addField("Read Chapter:", chapterLink)
+                .addField("Next Chapter Release", "~1 month")
+                .addField("Comment", (comment ? comment : "No upload comment because nothing cool happened."))
+                .setTimestamp(new Date())
+            client.channels.cache.get("856374936913838100").send(embed).then( () => {
+                client.channels.cache.get("856374936913838100").send(`<@&${"856375356494708766"}>`)
+            })
+        }
+
+        if (cmd === "shinja") {
+            if (args.length < 1) {
+              message.reply("no new chapter link? dumbasss...LMFAO")
+              return;
+            }
+
+            let chapterLink = args.splice(0, 1)
+            let comment = args.join(" ")
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(0x9af59f)
+                .setThumbnail("https://strapi.wehateisekai.club/uploads/49655_e030b16e36.png")
+                .setTitle("New Chapter")
+                .setDescription("There's a new chapter of Shinja!!!!!! OMG POG!!")
+                .addField("Read Chapter:", chapterLink)
+                .addField("Next Chapter Release", "~1 month")
+                .addField("Comment", (comment ? comment : "No upload comment because nothing cool happened."))
+                .setTimestamp(new Date())
+            client.channels.cache.get("856374950990446632").send(embed).then( () => {
+                client.channels.cache.get("856374950990446632").send(`<@&${"856375381303885924"}>`)
+            })
+        }
+        // console.log("admin user")
     }
 });
+
+function addRole(guild, emojiName, user) {
+    switch (emojiName) {
+        case '1️⃣':
+            role = guild.roles.cache.get('856375305921101836')
+            break;
+        case '2️⃣':
+            role = guild.roles.cache.get('856375356494708766')
+            break;
+        case '3️⃣':
+            role = guild.roles.cache.get('856375381303885924')
+            break;
+        default:
+            role = null
+    }
+
+    guild.members.fetch(user.id).then((m) => {
+        m.roles.add(role).catch((e) => {
+            console.error(e)
+        })
+    })
+}
+
+client.on('messageReactionAdd', async (message, user) => {
+    if (message.message.id != botConfig.roleMsg) return;
+    if (message.emoji.name != '1️⃣' && message.emoji.name != '2️⃣' && message.emoji.name != '3️⃣') return;
+
+    addRole(message.message.guild, message.emoji.name, user)
+})
+
+function removeRole(guild, emojiName, user) {
+    switch (emojiName) {
+      case '1️⃣':
+          role = guild.roles.cache.get('856375305921101836')
+          break;
+      case '2️⃣':
+          role = guild.roles.cache.get('856375356494708766')
+          break;
+      case '3️⃣':
+          role = guild.roles.cache.get('856375381303885924')
+          break;
+      default:
+          role = null
+    }
+
+    guild.members.fetch(user.id).then((m) => {
+        m.roles.remove(role).catch((e) => {
+            console.error(e)
+        })
+    })
+}
+
+client.on('messageReactionRemove', async (message, user) => {
+    if (message.message.id != botConfig.roleMsg) return;
+    if (message.emoji.name != '1️⃣' && message.emoji.name != '2️⃣' && message.emoji.name != '3️⃣') return;
+
+    removeRole(message.message.guild, message.emoji.name, user)
+})
 
 client.login(botConfig.token);
